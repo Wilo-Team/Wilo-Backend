2f9e75939e070d3e89677ee8ab1a669f568bb54b
seungin <tmddls91@inha.edu>
2026-01-22 23:51:28 +0900

    [init] 프로젝트 초기 세팅

---
+++ src/main/java/com/wilo/server/WiloApplication.java
@@ -0,0 +1,13 @@
+package com.wilo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiloApplication.class, args);
+	}
+
+}
