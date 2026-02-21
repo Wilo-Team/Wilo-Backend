732a534bceadbd95bf58e8523d21d1e79c120c8c
seungin <tmddls91@inha.edu>
2026-02-21 20:56:50 +0900

    [feat] 세션 관련 ErrorCase 추가

--- src/main/java/com/wilo/server/chatbot/exception/ChatbotErrorCase.java
+++ src/main/java/com/wilo/server/chatbot/exception/ChatbotErrorCase.java
@@ -11,7 +11,10 @@ public enum ChatbotErrorCase implements ErrorCase {
     CHATBOT_INTERNAL_SERVER_ERROR(500, 3001, "서버 오류가 발생했습니다."),
     CHATBOT_TYPE_NOT_FOUND(404, 3002, "존재하지 않는 챗봇 유형입니다."),
     INVALID_PARAMETER(400, 3003, "요청 파라미터가 올바르지 않습니다."),
-    GUEST_ID_REQUIRED(400, 3004, "비로그인 사용자는 X-Guest-Id 헤더가 필요합니다.");
+    GUEST_ID_REQUIRED(400, 3004, "비로그인 사용자는 X-Guest-Id 헤더가 필요합니다."),
+    SESSION_FORBIDDEN(403, 3005, "접근 권한이 없습니다."),
+    SESSION_NOT_FOUND(404, 3006, "대화 세션을 찾을 수 없습니다.");
+
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
