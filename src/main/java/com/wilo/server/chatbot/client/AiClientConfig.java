63031915ef3b3501a35d8584f0ad5d2facfdb42a
seungin <tmddls91@inha.edu>
2026-02-25 23:18:00 +0900

    [chore] WebClient 관련 의존성 추가

---
+++ src/main/java/com/wilo/server/chatbot/client/AiClientConfig.java
@@ -0,0 +1,8 @@
+package com.wilo.server.chatbot.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiClientConfig {
+}
--- src/main/java/com/wilo/server/chatbot/client/RealAiChatClient.java
+++ src/main/java/com/wilo/server/chatbot/client/RealAiChatClient.java
@@ -1,4 +1,9 @@
 package com.wilo.server.chatbot.client;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class RealAiChatClient {
 }
